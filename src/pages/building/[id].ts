6a1b83eb0bc9ed7fea58caa460664c8e7d6feed2
VladimirMoizes <vova3x20@gmail.com>
2026-04-09 21:08:57 +0300

    feat: add lift page

--- src/pages/building/[id].ts
+++ src/pages/building/[id].ts
@@ -1,34 +1,35 @@
-// function movingCar() {
-//   const car = document.querySelector('[data-car]') as HTMLElement;
-
-//   if (car) {
-//     setTimeout(() => {
-//       car.setAttribute('data-moving', 'true');
-//     }, 100);
-//   }
-// }
-
-// document.addEventListener('DOMContentLoaded', () => {
-//   movingCar();
-// });
-
-// document.addEventListener('astro:page-load', () => {
-//   movingCar();
-// });
-
 function movingCar() {
   if (typeof window === 'undefined') return;
 
   const car = document.querySelector('[data-car]') as HTMLElement;
+  const building = document.querySelector(
+    '[class*="mainBuilding"]'
+  ) as HTMLElement;
 
   if (car) {
     setTimeout(() => {
       car.setAttribute('data-moving', 'true');
     }, 100);
   }
+
+  if (building && window.location.pathname.includes('workshop')) {
+    const link = document.querySelector('[data-link]');
+    link?.setAttribute('href', '/lift');
+  }
+
+  if (building && window.location.pathname.includes('exhibition')) {
+    building.style.position = 'relative';
+    building.style.top = '-30px';
+    building.style.left = '30px';
+  }
+
+  if (building && window.location.pathname.includes('cosmoport')) {
+    building.style.position = 'relative';
+    building.style.top = '-10px';
+    building.style.left = '20px';
+  }
 }
 
-// Запускаем только в браузере
 if (typeof window !== 'undefined') {
   document.addEventListener('DOMContentLoaded', () => {
     movingCar();
