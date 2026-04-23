f5db7d5f4cfb53f49c8fa16b849b75073ef50ccb
VladimirMoizes <vova3x20@gmail.com>
2026-04-23 22:39:57 +0300

    feat: continue sites page

---
+++ src/data/sitesData.ts
@@ -0,0 +1,34 @@
+export const sites = [
+  {
+    title: 'Юридическая компания',
+    image: '/assets/images/sites/law_firm.png',
+  },
+  {
+    title: 'Новый вкус',
+    image: '/assets/images/sites/new_taste.jpg',
+  },
+  {
+    title: 'ДСК R-1',
+    image: '/assets/images/sites/DSC_R1.png',
+  },
+  {
+    title: 'Неон',
+    image: '/assets/images/sites/neon.jpg',
+  },
+  {
+    title: 'Юридическая компания',
+    image: '/assets/images/sites/law_firm.png',
+  },
+  {
+    title: 'Новый вкус',
+    image: '/assets/images/sites/new_taste.jpg',
+  },
+  {
+    title: 'ДСК R-1',
+    image: '/assets/images/sites/DSC_R1.png',
+  },
+  {
+    title: 'Неон',
+    image: '/assets/images/sites/neon.jpg',
+  },
+];
