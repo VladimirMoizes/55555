f219b5305f836bca0d35c49768b2928ff37b8974
VladimirMoizes <vova3x20@gmail.com>
2026-06-04 18:21:44 +0300

    feat: start portfolio page

---
+++ src/data/portfolioData.ts
@@ -0,0 +1,22 @@
+export const roadMapItems = [
+  {
+    id: 1,
+    name: 'КРУПЫ',
+    image: '/assets/images/sites/new_taste1.png',
+  },
+  {
+    id: 2,
+    name: 'НЕОН',
+    image: '/assets/images/sites/neon.jpg',
+  },
+  {
+    id: 3,
+    name: 'МОСЭНЕРГОСБЫТ',
+    image: '/assets/images/sites/Mosenergosbyt.png',
+  },
+  {
+    id: 4,
+    name: 'ДСК Р-1',
+    image: '/assets/images/sites/DSC_R11.png',
+  },
+];
