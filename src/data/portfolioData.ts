aa867845ac59a27cd79f55dac18504db254b2647
VladimirMoizes <vova3x20@gmail.com>
2026-06-09 22:49:19 +0300

    feat: add portfolioId page

--- src/data/portfolioData.ts
+++ src/data/portfolioData.ts
@@ -3,20 +3,47 @@ export const roadMapItems = [
     id: 1,
     name: 'КРУПЫ',
     image: '/assets/images/sites/new_taste1.png',
+    imageId: '/assets/images/portfolio/id/new_taste.png',
+    people: {
+      '/assets/images/portfolio/people/11.png': [100, -100],
+      '/assets/images/portfolio/people/12.png': [400, -100],
+      '/assets/images/portfolio/people/13.png': [700, -100],
+    },
+    siteUrl: 'https://ru.wikipedia.org/wiki/Заглавная_страница',
   },
   {
     id: 2,
     name: 'НЕОН',
     image: '/assets/images/sites/neon.jpg',
+    imageId: '/assets/images/sites/neon.jpg',
+    people: {
+      '/assets/images/portfolio/people/21.png': [100, -100],
+      '/assets/images/portfolio/people/22.png': [400, -100],
+      '/assets/images/portfolio/people/23.png': [700, -100],
+    },
+    siteUrl: 'https://ru.wikipedia.org/wiki/Заглавная_страница',
   },
   {
     id: 3,
     name: 'МОСЭНЕРГОСБЫТ',
     image: '/assets/images/sites/Mosenergosbyt.png',
+    imageId: '/assets/images/sites/Mosenergosbyt.png',
+    people: {
+      '/assets/images/portfolio/people/31.png': [100, -80],
+      '/assets/images/portfolio/people/32.png': [400, -80],
+      '/assets/images/portfolio/people/33.png': [700, -80],
+    },
+    siteUrl: 'https://ru.wikipedia.org/wiki/Заглавная_страница',
   },
   {
     id: 4,
     name: 'ДСК Р-1',
     image: '/assets/images/sites/DSC_R11.png',
+    imageId: '/assets/images/sites/DSC_R11.png',
+    people: {
+      '/assets/images/portfolio/people/41.png': [150, -80],
+      '/assets/images/portfolio/people/42.png': [550, -80],
+    },
+    siteUrl: 'https://ru.wikipedia.org/wiki/Заглавная_страница',
   },
 ];
