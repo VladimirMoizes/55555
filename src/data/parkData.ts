9b64b6308e583fb3cb23aabdef718e7ab835f74b
VladimirMoizes <vova3x20@gmail.com>
2026-06-13 16:08:44 +0300

    feat: park page start

---
+++ src/data/parkData.ts
@@ -0,0 +1,32 @@
+export const roadSites = [
+  {
+    id: 1,
+    name: 'НЕОН',
+    image: '/assets/images/park/neon.png',
+  },
+  {
+    id: 2,
+    name: 'РЕМОНТ',
+    image: '/assets/images/park/repair.png',
+  },
+  {
+    id: 3,
+    name: 'ГЛАВСПОРТ',
+    image: '/assets/images/park/glavsport.png',
+  },
+  {
+    id: 4,
+    name: 'ФасадСпецСтрой',
+    image: '/assets/images/park/construction.png',
+  },
+  {
+    id: 5,
+    name: 'НОВЫЙ ВКУС',
+    image: '/assets/images/park/new_taste.png',
+  },
+  {
+    id: 6,
+    name: 'ТЕХНОТРАНС',
+    image: '/assets/images/park/shipping.png',
+  },
+];
