1ef39905e203c77a509121bb348cd18b63cf26a6
VladimirMoizes <vova3x20@gmail.com>
2026-06-14 14:21:20 +0300

    feat: add board page

---
+++ src/data/boardData.ts
@@ -0,0 +1,22 @@
+export const board = [
+  {
+    name: 'Мосэнергосбыт',
+    image: '/assets/images/board/letter.avif',
+  },
+  {
+    name: 'ФасадСпецСтрой',
+    image: '/assets/images/board/letter.avif',
+  },
+  {
+    name: 'Лайнер',
+    image: '/assets/images/board/letter.avif',
+  },
+  {
+    name: 'Бизнеc Секьюрити',
+    image: '/assets/images/board/letter.avif',
+  },
+  {
+    name: 'Осетинские пироги',
+    image: '/assets/images/board/letter.avif',
+  },
+];
--- src/data/parkData.ts
+++ src/data/parkData.ts
@@ -3,30 +3,36 @@ export const roadSites = [
     id: 1,
     name: 'НЕОН',
     image: '/assets/images/park/neon.png',
+    site: 'https://ru.wikipedia.org/wiki/Фронтенд_и_бэкенд',
   },
   {
     id: 2,
     name: 'РЕМОНТ',
     image: '/assets/images/park/repair.png',
+    site: 'https://ru.wikipedia.org/wiki/Фронтенд_и_бэкенд',
   },
   {
     id: 3,
     name: 'ГЛАВСПОРТ',
     image: '/assets/images/park/glavsport.png',
+    site: 'https://ru.wikipedia.org/wiki/Фронтенд_и_бэкенд',
   },
   {
     id: 4,
     name: 'ФасадСпецСтрой',
     image: '/assets/images/park/construction.png',
+    site: 'https://ru.wikipedia.org/wiki/Фронтенд_и_бэкенд',
   },
   {
     id: 5,
     name: 'НОВЫЙ ВКУС',
     image: '/assets/images/park/new_taste.png',
+    site: 'https://ru.wikipedia.org/wiki/Фронтенд_и_бэкенд',
   },
   {
     id: 6,
     name: 'ТЕХНОТРАНС',
     image: '/assets/images/park/shipping.png',
+    site: 'https://ru.wikipedia.org/wiki/Фронтенд_и_бэкенд',
   },
 ];
