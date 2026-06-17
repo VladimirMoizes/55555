1faaa48d275427c9d320565b2ddd35427552ef02
VladimirMoizes <vova3x20@gmail.com>
2026-06-17 22:04:40 +0300

    fix: add correct letters on board and clickable title

--- src/data/boardData.ts
+++ src/data/boardData.ts
@@ -1,22 +1,22 @@
 export const board = [
   {
     name: 'Мосэнергосбыт',
-    image: '/assets/images/board/letter.avif',
+    image: '/assets/images/board/letter_energo.png',
   },
   {
     name: 'ФасадСпецСтрой',
-    image: '/assets/images/board/letter.avif',
+    image: '/assets/images/board/letter_facade.png',
   },
   {
     name: 'Лайнер',
-    image: '/assets/images/board/letter.avif',
+    image: '/assets/images/board/letter_liner.png',
   },
   {
     name: 'Бизнеc Секьюрити',
-    image: '/assets/images/board/letter.avif',
+    image: '/assets/images/board/letter_business.jpg',
   },
   {
     name: 'Осетинские пироги',
-    image: '/assets/images/board/letter.avif',
+    image: '/assets/images/board/letter_ossetian_pies.jpg',
   },
 ];
