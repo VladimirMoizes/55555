9828a7d234f9c9c6587f2cac6b639948e931f8fa
VladimirMoizes <vova3x20@gmail.com>
2026-08-23 18:58:19 +0300

    feat: add adaptive portfolio page

--- src/data/portfolioData.ts
+++ src/data/portfolioData.ts
@@ -1,7 +1,7 @@
 export const roadMapItems = [
   {
     id: 1,
-    name: 'КРУПЫ',
+    name: 'НОВЫЙ ВКУС',
     image: '/assets/images/sites/new_taste1.png',
     imageId: '/assets/images/portfolio/id/new_taste.png',
     people: {
@@ -45,5 +45,56 @@ export const roadMapItems = [
       '/assets/images/portfolio/people/42.png': [550, -80],
     },
     siteUrl: 'https://ru.wikipedia.org/wiki/Заглавная_страница',
+  },
+];
+
+export const mobilePeople = [
+  {
+    image: '/assets/images/portfolio/people/mob1.png',
+    position: { top: 40, left: 'clamp(-90px, -17.27px + -7.10vw, -40px)' },
+    width: 33,
+    height: 50,
+  },
+  {
+    image: '/assets/images/portfolio/people/11.png',
+    position: { top: 40, left: 'clamp(-95px, -22.27px + -7vw, -45px)' },
+    width: 40,
+    height: 40,
+  },
+  {
+    image: '/assets/images/portfolio/people/23.png',
+    position: { top: 30, left: 'clamp(-80px, -7.27px + -7.10vw, -30px)' },
+    width: 17,
+    height: 37,
+  },
+  {
+    image: '/assets/images/portfolio/people/22.png',
+    position: { top: 40, left: 'clamp(-90px, -17.27px + -7.10vw, -40px)' },
+    width: 33,
+    height: 33,
+  },
+  {
+    image: '/assets/images/portfolio/people/32.png',
+    position: { top: 35, left: 'clamp(-87px, -14.27px + -7.10vw, -37px)' },
+    width: 29,
+    height: 38,
+  },
+  {
+    image: '/assets/images/portfolio/people/33.png',
+    position: { top: 40, left: 'clamp(-82px, -9.27px + -7.10vw, -32px)' },
+    width: 19,
+    height: 35,
+  },
+  {
+    image: '/assets/images/portfolio/people/12.png',
+    position: { top: 70, left: 'clamp(-95px, -22.27px + -7.10vw, -45px)' },
+    width: 40,
+    height: 45,
+  },
+  {
+    image: '/assets/images/portfolio/people/31.png',
+    position: { top: 60, left: 'clamp(-80px, -7.27px + -7.10vw, -30px)' },
+    width: 21,
+    height: 39,
   },
 ];
